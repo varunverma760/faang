3fe1a61e5b88e01977d573a65c684fcfb9bb84d9
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2022-02-06 08:42:06 +0530

    Add files via upload

---
+++ CHARACTER ARRAYS/largest_string.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(){
+
+	int n;
+	
+	cin>>n;
+	cin.get();
+	
+	char sentence[1000];
+	char largest[1000];
+	
+	int largest_len=0;
+	
+	while(n--){
+		cin.getline(sentence,1000);
+		int len=strlen(sentence);
+		if (len>largest_len)
+		{
+			largest_len=len;
+			strcpy(largest,sentence);
+		}
+	}
+	cout<<"Largest Sentence is : "<<largest<<endl;
+ return 0;
+}
